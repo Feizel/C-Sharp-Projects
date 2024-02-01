2f9171f58a374361113b9135a1aa2b46df76441b
Feizel <feizel.maduna@gmail.com>
2024-02-01 13:24:14 +0200

    First iteration

--- MadLibGame/Program.cs
+++ MadLibGame/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MadLibGame
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            string colour, plural_noun, celebrity;
+
+            Console.WriteLine("Enter a colour: ");
+            colour = Console.ReadLine();
+
+            Console.WriteLine("Enter a plural noun: ");
+            plural_noun = Console.ReadLine();
+
+            Console.WriteLine("Enter a celebrity\'s name that you like: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are {colour}");
+            Console.WriteLine("{plural_noun} are blue");
+            Console.WriteLine("I love {celebrity}");
+
+        }
+    }
+
+}
